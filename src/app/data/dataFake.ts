8ff2f674b9fc0976510d573ddccc67c07d4b80e4
Allyson Araujo <allysoneugenio@gmail.com>
2025-04-03 12:06:40 -0300

    Atualizando noticias recentes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,7 +8,18 @@ export const dataFake = [
   {
     "id":"2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla bla bla bla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus.jpg"
+    "description": "Paradise, nova série de suspense político, já está disponível no Disney+",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F63BA38D4A0568A13A861A7E5ACA887F12B4DDC9B1E8E6B3E05C5E88CB1C25B8/scale?width=440&aspectRatio=1.78&format=webp"
+  },
+  { "id":"3",
+    "title": "Lançamento do Filme: Pantera Negra",
+    "description": "Lançamento em Novembro de 2022",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/E4475104C984F5D9F6E97DC692DB0FAD25980507FE06A7B4550371096397E4A5/scale?width=1200&aspectRatio=1.78&format=webp"
+  },
+  { "id":"4",
+    "title": "Filme do X-Men em pós-produção",
+    "description": "Diretor da Marvel afirma que está nos preparativos finais.",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/820x547/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+
   },
 ]
